1a42fc301978eb1945ceabc67a0f958f011c2fff
m-maaz-azhar <maazazher28@gmail.com>
2024-05-25 16:36:30 +0500

    Online Payment Fix

--- App/components/RadioButton/index.js
+++ App/components/RadioButton/index.js
@@ -3,9 +3,9 @@ import {View, TouchableOpacity} from 'react-native';
 import {SvgXml} from 'react-native-svg';
 import {RadioButtonIcon, RadioButtonIconActive} from '../../assets/images';
 
-const RadioButton = ({isCheck, style, onPress}) => {
+const RadioButton = ({isCheck, style, onPress, disabled}) => {
   return (
-    <TouchableOpacity activeOpacity={0.54} style={{...style}} onPress={onPress}>
+    <TouchableOpacity activeOpacity={0.54} style={{...style, opacity: disabled ? 0.3 :1}} onPress={ disabled ? null : onPress}>
       {isCheck ? (
         <View>
           <SvgXml xml={RadioButtonIconActive} width={20} height={20} />
